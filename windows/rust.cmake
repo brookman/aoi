92e8718501e9c8e5998f018281df205b5288a487
Benjamin Bachmann <benjamin.bachmann@zuehlke.com>
2023-02-04 21:48:15 +0100

    Rename rust project from "native" to "aoi".

--- windows/rust.cmake
+++ windows/rust.cmake
@@ -14,11 +14,11 @@ FetchContent_Declare(
 
 FetchContent_MakeAvailable(Corrosion)
 
-corrosion_import_crate(MANIFEST_PATH ../native/Cargo.toml)
+corrosion_import_crate(MANIFEST_PATH ../aoi/Cargo.toml)
 
 # Flutter-specific
 
-set(CRATE_NAME "native")
+set(CRATE_NAME "aoi")
 
 target_link_libraries(${BINARY_NAME} PRIVATE ${CRATE_NAME})
 
